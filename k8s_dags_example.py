08241d9ce4ff8423bb63c341b4f3a63dbc729a03
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2022-09-27 18:57:53 -0300

    dag

--- k8s_dags_example.py
+++ k8s_dags_example.py
@@ -1,6 +1,10 @@
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
 
+dag = DAG('hello_world_pod_operator', description='Hello World DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
 k = KubernetesPodOperator(
     name="hello-dry-run",
     image="debian",
@@ -8,5 +12,5 @@ k = KubernetesPodOperator(
     arguments=["echo", "10"],
     labels={"foo": "bar"},
     task_id="dry_run_demo",
-    do_xcom_push=True,
+    dag=dag
 )
