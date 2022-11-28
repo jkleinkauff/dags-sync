7a68977ad10256dd419936b8f56d214e0cf0ac2d
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2022-11-28 15:39:40 -0300

    new dag to test spark submit

---
+++ spark_job.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+
+dag = DAG(
+    "spark_job",
+    description="Hello World Spark DAG",
+    schedule_interval="0 */6 * * *",
+    start_date=datetime(2017, 3, 20),
+    catchup=False,
+)
+
+k = KubernetesPodOperator(
+    task_id='spark-job-task-ex',
+    name='spark-job-task',
+    namespace='airflow',
+    image='kleinkauff/spark-py',
+    cmds=['bin/spark-submit'],
+    arguments=[
+        '--master k8s://https://192.168.15.180:6443',
+        #'--deploy-mode cluster',
+        '--name spark-pi',
+        ' --class org.apache.spark.examples.SparkPi',
+        '--conf spark.executor.instances=3',
+        '--conf spark.kubernetes.container.image=kleinkauff/spark-py',
+        '$SPARK_HOME/examples/src/main/python/pi.py'
+    ],
+    dag=dag,
+)
