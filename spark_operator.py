7f9c49f9b5c71b8c00a3deef71313668c151c2f7
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2022-11-28 18:58:28 -0300

    fix

--- spark_operator.py
+++ spark_operator.py
@@ -29,9 +29,14 @@ from datetime import timedelta, datetime
 # [START import_module]
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
+
 # Operators; we need this to operate!
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import (
+    SparkKubernetesOperator,
+)
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
+    SparkKubernetesSensor,
+)
 from airflow.utils.dates import days_ago
 
 # [END import_module]
@@ -40,48 +45,47 @@ from airflow.utils.dates import days_ago
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'max_active_runs': 1,
-    'retries': 3
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "max_active_runs": 1,
+    "retries": 3,
 }
 # [END default_args]
 
 # [START instantiate_dag]
 
 dag = DAG(
-    'spark_pi',
+    "spark_pi",
     default_args=default_args,
     schedule_interval=timedelta(days=1),
-    tags=['example']
+    tags=["example"],
 )
 
 # spark = open(
 #     "example_spark_kubernetes_operator_pi.yaml").read()
 
 submit = SparkKubernetesOperator(
-    task_id='spark_pi_submit',
+    task_id="spark_pi_submit",
     namespace="airflow",
     application_file="spark/config.yaml",
     kubernetes_conn_id="kubernetes_in_cluster",
     do_xcom_push=True,
     dag=dag,
     # api_group="sparkoperator.hpe.com",
-    enable_impersonation_from_ldap_user=False
 )
 
 sensor = SparkKubernetesSensor(
-    task_id='spark_pi_monitor',
+    task_id="spark_pi_monitor",
     namespace="airflow",
     application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
     kubernetes_conn_id="kubernetes_in_cluster",
     dag=dag,
     # api_group="sparkoperator.hpe.com",
-    attach_log=True
+    attach_log=True,
 )
 
 submit >> sensor
