6bf6d103c3d8be48a6567d30122002d15c17f5fc
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2022-11-30 11:03:35 -0300

    fix

--- spark_operator.py
+++ spark_operator.py
@@ -7,12 +7,30 @@ from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
     SparkKubernetesSensor,
 )
 from airflow.utils.dates import days_ago
+from airflow.models import Variable
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
 
-# [END import_module]
+aws_access_key = Variable.get("AWS_ACCESS_KEY")
 
-# [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
+
+#Replacing env vars in config.yaml - This is FAR from recommended. 
+# The ideal solution - for spark operator - is to enable a WebHook in k8s cluster
+# If not enabled, you need to use the env vars like I'm doing here
+
+aws_cred = AwsBaseHook("aws-spark", client_type="s3").get_credentials()
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=aws_cred.access_key,
+    aws_secret_access_key=aws_cred.secret_key,
+)
+
+s3_client.download_file(f"data-lake-jho", "spark-jobs/config.yaml", "config.yaml")
+
+with open('config.yaml', 'r') as file :
+  yaml_data = file.read()
+
+yaml_data = yaml_data.replace('@AWS_ACCESS_KEY', 'aws_access_key')
+
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -40,7 +58,8 @@ dag = DAG(
 submit = SparkKubernetesOperator(
     task_id="spark_pi_submit",
     namespace="airflow",
-    application_file="spark/config.yaml",
+    # application_file="spark/config.yaml",
+    application_file=yaml_data
     kubernetes_conn_id="kubernetes_in_cluster",
     do_xcom_push=True,
     dag=dag,
